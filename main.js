f35edf40f98e07e122347727b440641a36ef563c
pilevsky <pilevskiyd@yandex.ru>
2023-08-27 00:51:02 +0600

    ToDoList3

--- main.js
+++ main.js
@@ -3,20 +3,38 @@ let taskInput = document.querySelector(".text");
 let plus = document.querySelector(".plus");
 let noTaskMsg = document.querySelector(".no-tasks-message");
 let taskList = document.querySelector(".tasks-content");
+let toggleCheckbox = document.querySelector("#toggle_checkbox");
+let body = document.querySelector("body");
+let tasksCount = [];
+let tasksCompleted = [];
 
-let arr = [];
+toggleCheckbox.onclick = function () {
+  if (getComputedStyle(body).backgroundColor == "rgba(135, 143, 229, 0.5)") {
+    body.style.backgroundColor = "#181818";
+    document.querySelector(".add-task").style.backgroundColor = "#282828";
+    document.querySelector(".tasks-content").style.backgroundColor = "#282828";
+    document.querySelector(".task-stats").style.backgroundColor = "#282828";
+    document.querySelector(".task-stats").style.color = "white";
+    document.querySelector(".no-tasks-message").style.color = "white";
+  } else {
+    body.style.backgroundColor = "rgba(135, 143, 229, 0.5)";
+    document.querySelector(".add-task").style.backgroundColor = "#878fe5";
+    document.querySelector(".tasks-content").style.backgroundColor = "#878fe5";
+    document.querySelector(".task-stats").style.backgroundColor = "#878fe5";
+  }
+};
 
-// arr.push("1");
-  
+plus.addEventListener("click", addTasks);
+document.addEventListener("keyup", addTasks);
 
-
-  
-plus.addEventListener("click", function (event) {
-  event.preventDefault();
-  let taskText = taskInput.value;
-  let taskHTML = `<div class="card">
-    <ul id="tasks-content" class="list-group list-group-flush">
-             <li class="list-group-item d-flex justify-content-between task-item">
+function addTasks(event) {
+  if (event.code === "Enter" || event.target.tagName == "SPAN") {
+    if (taskInput.value.length >= 1) {
+      event.preventDefault();
+      let taskText = taskInput.value;
+      let taskHTML = `<div class="card">
+    <ul id="tasks-content">
+             <li class="list-group-item">
             <span class="task-title">${taskText}</span>
             <div class="task-item__buttons">
                 <button type="button" data-action="done" class="btnDone">
@@ -29,29 +47,35 @@ plus.addEventListener("click", function (event) {
         </li>
     </ul>
 </div>`;
-  taskList.insertAdjacentHTML("beforeend", taskHTML);
-  taskInput.value = "";
-  taskInput.focus();
-  arr.push("1");
-  let count = document.querySelector(".count");
-  //  count.innerHTML = taskList.children.length + 1;
-  count.innerHTML = arr.length;
-  if (taskList.children.length > 1) {
-    noTaskMsg.classList.add("none");
+      taskList.insertAdjacentHTML("beforeend", taskHTML);
+      taskInput.value = "";
+      taskInput.focus();
+      tasksCount.push("1");
+      let count = document.querySelector(".count");
+      count.innerHTML = tasksCount.length;
+      if (taskList.children.length > 1) {
+        noTaskMsg.classList.add("none");
+      }
+    } else {
+      taskInput.focus();
+    }
   }
-});
+}
 
 taskList.addEventListener("click", deleteTask);
 
 taskList.addEventListener("click", doneTask);
 
 function deleteTask(event) {
-  if (event.target.dataset.action == "delete") { //удаляет первое задание!
-    document.querySelector(".card").remove();
-    console.log(taskList.children.length);
-      arr.pop("1");
+  if (event.target.dataset.action == "delete") {
+    let parentNode = event.target.closest(".card");
+    parentNode.remove();
+    tasksCount.pop("1");
     let count = document.querySelector(".count");
-    count.innerHTML = arr.length;
+    count.innerHTML = tasksCount.length;
+    tasksCompleted.pop("1");
+    let completed = document.querySelector(".completed");
+    completed.innerHTML = tasksCompleted.length;
   }
 
   if (taskList.children.length === 1) {
@@ -68,10 +92,14 @@ function doneTask(event) {
       "line-through solid rgb(0, 0, 0)"
     ) {
       taskTitle.style = "text-decoration:none";
+      tasksCompleted.pop("1");
+      let completed = document.querySelector(".completed");
+      completed.innerHTML = tasksCompleted.length;
     } else {
       taskTitle.style = "text-decoration:line-through";
+      let completed = document.querySelector(".completed");
+      tasksCompleted.push("1");
+      completed.innerHTML = tasksCompleted.length;
     }
   }
 }
-
-
